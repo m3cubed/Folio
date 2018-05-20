77b145288c410b7244b5a921da07a80ca5ebeee0
oathkeeper40 <mike.ma0426@gmail.com>
2018-05-20 16:19:43 -0400

    May. 20th Main
    - Quick fixes

--- src/components/Quill.js
+++ src/components/Quill.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactQuill, { Quill } from "react-quill";
+import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css"; // ES6
 import "react-quill/dist/quill.bubble.css";
 import "./Quill.css";
